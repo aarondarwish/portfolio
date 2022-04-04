96db7553436a842b96da932d265c3a4ef76d9c43
Aaron Darwish <aaron7darwish@gmail.com>
2022-04-04 17:07:41 +0100

    test

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -3,19 +3,15 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [{
-  title: 'Software Development', description: 'I am a decent engineer.', imgUrl: images.about01
-},
-{
-  title: 'Sysadmin', description: 'I am a decent engineer.', imgUrl: images.about02
-},
-{
-  title: 'UI/UX', description: 'I am a decent engineer.', imgUrl: images.about03
-}
-]
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className='head-text'>I know that <span>Good Development</span> <br /> means <span>Good Business</span>
@@ -30,9 +26,9 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
-            <h2 className='bold-text' style={{ marginTop: 20}}>{about.title}</h2>
-            <p className='p-text' style={{ marginTop: 10}}>{about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
 
         ))}
