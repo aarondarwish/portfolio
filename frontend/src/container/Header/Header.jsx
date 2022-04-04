bf8dfe4964b507ccf09be75a20eb65ce71b61454
Aaron Darwish <aaron7darwish@gmail.com>
2022-04-04 12:39:34 +0100

    Navigation bar, and fading effects.

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 
 import './Header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='app__header app__flex'>
+      <motion.div whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 1.3 }} className='app__header-info'>
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{ marginLeft: 20}}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Aaron Darwish</h1>
+            </div>
+          </div>
+        </div>
+
+      </motion.div>
+    </div>
   )
 }
 
