0cb7690ce8f006ef09ff6e3e7635480454a6a188
Aaron Darwish <aaron7darwish@gmail.com>
2022-04-05 09:08:58 +0100

    Starting the experiences section of the skills segment.

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -22,14 +22,23 @@ const Work = () => {
   }, []);
 
   const handleWorkFilter = (item) => {
-    // setAnimateCard([])
+    setActiveFilter(item);
+    setAnimateCard([{ y: 100, opacity: 0 }]);
+
+    setTimeout(() => {
+      setAnimateCard([{ y: 0, opacity: 1 }]);
+
+      if (item === 'All') {
+        setFilterWork(works);
+      } else setFilterWork(works.filter((work) => work.tags.includes(item)));
+    }, 500);
   }
 
   return (
     <>
       <h2 className='head-text'>My Creative <span>Portfolio</span> Section</h2>
       <div className='app__work-filter'>
-        {['UI/UX', 'Web App', 'Mobile App'].map((item, index) => (
+        {['All', 'UI/UX', 'Web App', 'Mobile App'].map((item, index) => (
           <div key={index}
             onClick={() => handleWorkFilter(item)}
             className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
